fff3786eba835b4360d6314d6b1b62e2bd9bcc80
AlexsJones <alexsimonjones@gmail.com>
2013-02-04 13:49:32 +0000

    fixed some of the mental indentation

--- dbconnect.rb
+++ dbconnect.rb
@@ -2,32 +2,32 @@ require 'mysql2'
 require 'yaml'
 
 class Dbconnect
-	
-	def initialize
-	  begin
-      dbconfig = YAML.load_file("conf/db.conf")
 
-      @host = dbconfig['host']
-      @user = dbconfig['username']
-      @password = dbconfig['password']
-      @database = dbconfig['database']
-      
-      print "[DEBUG] @host = #{@host}, @user = #{@user}, @password = #{@password}, @database = #{@database}\n"
-    rescue
-      print "\n"
-      print "ERROR: It appears that you either don't have conf/db.conf file or that it couldn't be opened.\n"
-      print "If you need to create the file then the easiest thing to do is to copy conf/db_conf.example,\n"
-      print "rename the copy to db.conf, and change the values to match your MySQL database configuration.\n"
-      print "\n"
-      
-      raise
-    end
-  end
-  
+	def initialize
+		begin
+			dbconfig = YAML.load_file("conf/db.conf")
+
+			@host = dbconfig['host']
+			@user = dbconfig['username']
+			@password = dbconfig['password']
+			@database = dbconfig['database']
+
+			print "[DEBUG] @host = #{@host}, @user = #{@user}, @password = #{@password}, @database = #{@database}\n"
+		rescue
+			print "\n"
+			print "ERROR: It appears that you either don't have conf/db.conf file or that it couldn't be opened.\n"
+			print "If you need to create the file then the easiest thing to do is to copy conf/db_conf.example,\n"
+			print "rename the copy to db.conf, and change the values to match your MySQL database configuration.\n"
+			print "\n"
+
+			raise
+		end
+	end
+
 	def query(inputquery)
 		client = Mysql2::Client.new(:host => @host, :username => @user, :password =>  @password, :database => @database, :async => true)
 		results = client.query(inputquery)
-    
+
 		return results
 	end
 end
--- jobman.rb
+++ jobman.rb
@@ -3,20 +3,20 @@ require 'tempfile'
 require 'yaml'
 
 class Jobman
-  
-  def initialize
-    begin
-      config = YAML.load_file("conf/general.conf")
-    
-      @timewait = config['timewait'] && 10
-      @satellite_port = config['satellite_port'] && 9099
-    rescue
-      @timewait = 10
-      @satellite_port = 9099
-    end
-    
-    print "@timewait = #{@timewait}, @satellite_port = #{@satellite_port}"
-  end
+
+	def initialize
+		begin
+			config = YAML.load_file("conf/general.conf")
+
+			@timewait = config['timewait'] && 10
+			@satellite_port = config['satellite_port'] && 9099
+		rescue
+			@timewait = 10
+			@satellite_port = 9099
+		end
+
+		print "@timewait = #{@timewait}, @satellite_port = #{@satellite_port}"
+	end
 
 	def main_loop
 		while true
@@ -25,30 +25,30 @@ class Jobman
 			incomplete = Hound.get_unstarted_jobs
 			if incomplete.size > 0
 				puts "Awaiting completion"
-				
+
 				incomplete.each do | entry |
-				m =  Hound.directquery("select machine_ip from `AUTOMATION`.`machines` where machine_id=#{entry['machines_machine_id']}")
+					m =  Hound.directquery("select machine_ip from `AUTOMATION`.`machines` where machine_id=#{entry['machines_machine_id']}")
 
-					puts "JOB------------"
-					puts "Job id #{entry['id']}"
-					puts "Machine IP #{entry['machines_machine_id']}"
-					
-					#This is because the result is returned to us as an array of hashes..
-					puts m.first["machine_ip"]
+				puts "JOB------------"
+				puts "Job id #{entry['id']}"
+				puts "Machine IP #{entry['machines_machine_id']}"
 
-					puts "Command #{entry['command']}"
-					puts "---------------"
-					tmp = Tempfile.new("#{entry['id']}")
-					tmp << "#{entry['command']}"
-					tmp.close
+				#This is because the result is returned to us as an array of hashes..
+				puts m.first["machine_ip"]
+
+				puts "Command #{entry['command']}"
+				puts "---------------"
+				tmp = Tempfile.new("#{entry['id']}")
+				tmp << "#{entry['command']}"
+				tmp.close
 				#lets feed this information to our satellite
-					puts request = "satellite -m SEND -h #{m.first['machine_ip']} -p #{@satellite_port} -i #{tmp.path} -j #{entry['id']}"
-					system request
-					#Lets now update the job progress 
-					Hound.set_job_progress(entry['id'])
+				puts request = "satellite -m SEND -h #{m.first['machine_ip']} -p #{@satellite_port} -i #{tmp.path} -j #{entry['id']}"
+				system request
+				#Lets now update the job progress 
+				Hound.set_job_progress(entry['id'])
 				end
 			end
-			
+
 		end
 	end
 

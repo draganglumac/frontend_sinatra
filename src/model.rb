820b1462d45cce25f1766b9d0a243758fb63ac46
Cococoder <delaney.burke@me.com>
2013-02-28 09:46:34 +0000

    push for alex

---
+++ src/model.rb
@@ -0,0 +1,51 @@
+module AutomationStack
+
+	module Application
+
+	end
+
+	module Domain
+		
+	end
+
+
+	module Infrastructure
+
+	class Machines < Sequel::Model
+			one_to_many :jobs
+			many_to_one :Platform
+			one_to_many :connected_devices
+		end
+
+		class Analytics < Sequel::Model
+
+		end
+
+		class Platform < Sequel::Model
+			one_to_many :devices
+			one_to_many :machines
+		end
+
+		class ConnectedDevices < Sequel::Model
+			many_to_one :machine
+			many_to_one :device
+		end
+
+
+		class Devices < Sequel::Model
+			
+		end
+
+		class Jobs < Sequel::Model
+			many_to_one :job
+			many_to_one :results
+
+		end
+
+		class Result < Sequel::Model
+			many_to_one :job
+		end
+	
+	end
+	
+end

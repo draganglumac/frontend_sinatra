e5ac543d118a72f9a7c0ba66d2f96d9c4d2ff17f
Cococoder <delaney.burke@me.com>
2013-03-12 06:48:10 +0000

    now with session unit tests

--- tests/sessions_tests.rb
+++ tests/sessions_tests.rb
@@ -1,5 +1,28 @@
 require_relative "_test_strap"
 
 context "AutomationStack" do
-  setup { get '/' }
+  context "session" do
+  	context "valid username and password" do
+  	  setup  { post '/session',params={:username => "delaney.burke", :password => "sky"}}
+  	  asserts("valid username and password redirects") do  	  	
+  	  	topic.status == 302 and !topic.location.include? "login=failed"
+  	  end
+  	end
+
+  	context "invalid username and password" do
+  		context "password" do
+  			setup { post '/session',params={:username => "delaney.burke", :password => "invalid"}}
+  			asserts("invalid password") { topic.status == 302 and topic.location.include? "login=failed" }
+  		end
+  		
+  		context "username" do
+  		  setup { post '/session',params={:username => "invalid", :password => "sky"}}
+  		  asserts("invalid password") { topic.status == 302 and topic.location.include? "login=failed" }
+  		end
+  		
+
+  	end
+  	
+  end
+  
 end

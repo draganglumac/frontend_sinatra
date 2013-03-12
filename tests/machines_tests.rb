5163b6930f09dc563526509889f1479adf944c1c
Cococoder <delaney.burke@me.com>
2013-03-12 12:34:54 +0000

    now with jobs list

--- tests/machines_tests.rb
+++ tests/machines_tests.rb
@@ -3,9 +3,18 @@ require_relative "_test_strap"
 context "Automation Stack" do
 	context "ui" do
 	  context "machines" do
+
+	  	context "list" do
+	  	  setup { get "/" }
+	  	  asserts("has list"){topic.ok?}
+	  	end
 	    context "Add" do
 	      setup { post '/machines',params={:call_sign => "iceman", :ip_address => "127.0.0.1", :platform_id => "1"}}
-	      asserts("new job should exist") {topic.redirect?}
+	      asserts("new job should exist") do
+	      	page = follow_redirect!
+			page.body.include? "Machine Callsign"
+	      	
+	      end
 	    end
 	     
 	  end

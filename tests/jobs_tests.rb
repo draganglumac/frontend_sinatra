896a8979a958cc5070f8e100b9fdbf8c9579bc02
Cococoder <delaney.burke@me.com>
2013-03-12 10:10:35 +0000

    now with add job

--- tests/jobs_tests.rb
+++ tests/jobs_tests.rb
@@ -8,8 +8,8 @@ context "AutomationStack" do
 	  end
 
 	  context "add job" do
-	  #	setup { post "/job",params =>{"file_source" => Rack::Test::UploadedFile.new("/Users/cococoder/Desktop/sky/example.conf", "application/octet-stream"),:lname => "test", :ltrigger => "12/03/2013 07:07:27.000000", :machine_id => "1"} }
-
+	  setup { post "/job",params={"file_source" => Rack::Test::UploadedFile.new("/Users/cococoder/Desktop/sky/example.conf", "application/octet-stream"),:lname => "test", :ltrigger => "12/03/2013 07:07:27.000000", :machine_id => "1","is_private" => "0"} }
+	  asserts("can add job") { topic.redirect?}
 	  end
 	end
 

b2db4f205b20ca083470fd4b0b19d7482a05a766
Caspar <admin@caspar.local>
2013-04-03 14:05:14 +0100

    made new api for posting file uploads and gutted out uploads folder

--- server.rb
+++ server.rb
@@ -35,142 +35,156 @@ set :environment, :development
 set :public_folder, 'public'
 set :views ,'views'
 
- enable :sessions  
+enable :sessions  
 
 configure :test,:development do
-  Pony.options = {
-    :via => :smtp,
-    :via_options => {
-      :address => '127.0.0.1',
-      :port => '1025'
-    }
-  }
+	Pony.options = {
+		:via => :smtp,
+		:via_options => {
+		:address => '127.0.0.1',
+		:port => '1025'
+	}
+	}
 end
-                           
+
 @@connections = []
 #instantiate our database connection
 #Entry page
 
 helpers do
 
-    def partial(page, options={})
-     erb page, options.merge!(:layout => false)
-    end
+	def partial(page, options={})
+		erb page, options.merge!(:layout => false)
+	end
 
-    def current_user
-        session[:current_user]
-    end
+	def current_user
+		session[:current_user]
+	end
 
-    
 
-    def can_edit?
-        current_user
-    end
-    
-    def is_admin?
-        can_edit?
-    end
-        
-    def get_files(path)
-        dir_array = Array.new
-        Find.find(path) do |f|
-            dir_array << File.basename(f, ".*")
-        end
-        return dir_array
-    end 
-    
-    def suported_platorm(machine)
-      case machine.supported_platforms
-      when "ios"
-        return "/images/ios.png"
-      when "android"
-        return "/images/android.png"
-      end
-      
-      raise "oooops! #{machine.supported_platforms} is not supported !"
-    end
+
+	def can_edit?
+		current_user
+	end
+
+	def is_admin?
+		can_edit?
+	end
+
+	def get_files(path)
+		dir_array = Array.new
+		Find.find(path) do |f|
+			dir_array << File.basename(f, ".*")
+		end
+		return dir_array
+	end 
+
+	def suported_platorm(machine)
+		case machine.supported_platforms
+		when "ios"
+			return "/images/ios.png"
+		when "android"
+			return "/images/android.png"
+		end
+
+		raise "oooops! #{machine.supported_platforms} is not supported !"
+	end
 end
 
 
 get '/home' do  
-    redirect '/'
+	redirect '/'
 end
 
 #landing page
 get '/' do
-    #@info = "Welcome #{request.ip}"
-    Hound.add_visitor(request.ip)
-    erb :home
+	#@info = "Welcome #{request.ip}"
+	Hound.add_visitor(request.ip)
+	erb :home
 end
 #resource page
 get '/resources' do
-    erb :installer
+	erb :installer
 end
 
 
 #results page
 get '/results' do
-    @time = Time.new.inspect
-    @results = Hound.get_results
-    erb :results
+	@time = Time.new.inspect
+	@results = Hound.get_results
+	erb :results
 end
 get '/analytics' do
 
-    @num_visitors = Hound.get_visitors
-    @num_completed_jobs = Hound.get_complete_jobs.size.to_i   
-    #puts @num_completed_jobs
-    @num_unstarted_jobs = Hound.get_unstarted_jobs.size.to_i   
-    #puts @num_unstarted_jobs
-    @num_pending_jobs = Hound.get_incomplete_jobs.size.to_i   
-    erb :analytics
+	@num_visitors = Hound.get_visitors
+	@num_completed_jobs = Hound.get_complete_jobs.size.to_i   
+	#puts @num_completed_jobs
+	@num_unstarted_jobs = Hound.get_unstarted_jobs.size.to_i   
+	#puts @num_unstarted_jobs
+	@num_pending_jobs = Hound.get_incomplete_jobs.size.to_i   
+	erb :analytics
 end
-put '/result/upload/:id' do
-  
-    job = AutomationStack::Infrastructure::Job.find(:id => params[:id])
-    
-    Dir.chdir("public/uploads") do
-      Dir.mkdir job.name unless Dir.exists? job.name
 
-      Dir.chdir("#{job.name}") do 
-        File.open("#{Time.now.to_i}.#{params[:result]}", 'w+') do |file|
-            file.write(request.body.read)
-          
-        end 
-      end
+post '/upload/:id/:filename' do
+	job = AutomationStack::Infrastructure::Job.find(:id => params[:id])
+	fu = params[:filename]
+	Dir.chdir("public/uploads") do
+		Dir.mkdir job.name unless Dir.exists? job.name
 
-    end
-    "ok"
+		Dir.chdir("#{job.name}") do 
+			File.open("#{Time.now.to_i}.#{fu}", 'w+') do |file|
+				file.write(request.body.read)
+
+			end 
+		end
+	end
+	put '/result/upload/:id' do
+
+		job = AutomationStack::Infrastructure::Job.find(:id => params[:id])
+
+		Dir.chdir("public/uploads") do
+			Dir.mkdir job.name unless Dir.exists? job.name
+
+			Dir.chdir("#{job.name}") do 
+				File.open("#{Time.now.to_i}.#{params[:result]}", 'w+') do |file|
+					file.write(request.body.read)
+				end          
+			end 
+		end
+
+	end
+	"ok"
 end
 #/uploads/hudsoniPhoneExample/cuke.html
 post '/results/:id' do
-    job_name = ""
-    Hound.directquery("select name from `jobs` where id=#{params[:id]}").each do |row|
-        #Just FYI, this select should only ever return 1 entry but its a hash so has to be enumerated as direct key purchase doesn't seem to do anything... though my ruby is pretty awful'
-        job_name = row
-    end
-    puts "Job name is #{job_name['name']}"
-    send_file("public/uploads/#{job_name['name']}/cuke.html")
+	job_name = ""
+	Hound.directquery("select name from `jobs` where id=#{params[:id]}").each do |row|
+		#Just FYI, this select should only ever return 1 entry but its a hash so has to be enumerated as direct key purchase doesn't seem to do anything... though my ruby is pretty awful'
+		job_name = row
+	end
+	puts "Job name is #{job_name['name']}"
+	send_file("public/uploads/#{job_name['name']}/cuke.html")
 end
 #system dashboard
 get '/dashboard' do
-    @current_jobs=Hound.get_jobs 
-    erb :dashboard
+	@current_jobs=Hound.get_jobs 
+	erb :dashboard
 end
 
 get '/contact' do
-    erb :contact
+	erb :contact
 end
 
 post '/contact' do    
-  
-  if params[:send]
-    beacon = Beacon.new
-    beacon.deliver(params[:subject],params[:priority],params[:description]) 
-    flash[:info] = "thank you! your request has been sent"
-  else
-    flash[:info] = "canceled !!"
-  end
-  
-  redirect '/contact'
-  
+
+	if params[:send]
+		beacon = Beacon.new
+		beacon.deliver(params[:subject],params[:priority],params[:description]) 
+		flash[:info] = "thank you! your request has been sent"
+	else
+		flash[:info] = "canceled !!"
+	end
+
+	redirect '/contact'
+
 end

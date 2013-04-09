bac6c0a348e584d7c84077ba1a952b70b4d3dc1c
Balthazar <balthazar@balthazar.local>
2013-04-09 15:22:15 +0100

    added a handler for a result existing without a results folder

--- server.rb
+++ server.rb
@@ -63,12 +63,16 @@ helpers do
 		target = "public/uploads"
 
 		Dir.chdir(target) do
-			Dir.chdir(name) do 
-				Dir.glob("**").each do|f|
-					epoch, filename = f.split('.', 2)
-					display_name = Time.at(epoch.to_i).to_datetime.strftime("%Y-%m-%d %H:%M:%S ") + filename
-					yield  "<a href=\"/uploads/#{name}/#{f}\">#{display_name}</a>"
+			if Dir.exist?(name)
+				Dir.chdir(name) do 
+					Dir.glob("**").reverse.each do|f|
+						epoch, filename = f.split('.', 2)
+						display_name = Time.at(epoch.to_i).to_datetime.strftime("%Y-%m-%d %H:%M:%S ") + filename
+						yield  "<a href=\"/uploads/#{name}/#{f}\">#{display_name}</a>"
+					end
 				end
+			else
+				status 500
 			end
 		end
 	end

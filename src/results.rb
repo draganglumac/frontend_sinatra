b5501c066ad1d0f797a53b199ca5693f301d3109
draganglumac <dragan.glumac@gmail.com>
2013-07-14 10:18:28 +0100

    Improved page loading for other files. Now loading them on demand using jQuery AJAX.

--- src/results.rb
+++ src/results.rb
@@ -46,10 +46,16 @@ module Results
       @project = project_name_from_job_name(job.name)
       @device = device_name_from_job_name(job.name)
       @main_paths = link_main_results(params[:job_id], "cukes.html")
-      @supporting_paths = link_supporting_results(params[:job_id], "cukes.html")
       
       erb :'results/resultjob', :layout => :results_layout
-    end	
+    end
+
+    get '/results/other/:job_id/:epoch' do
+      supporting_paths = link_supporting_results(params[:job_id], params[:main_file])
+      @supporting_paths = supporting_paths[params[:epoch]]
+
+      erb :'results/other_results', :layout => false
+    end
   end
 
 end

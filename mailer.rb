9247cd7b72be40da43598c8b931f73f568290a8e
draganglumac <dragan.glumac@gmail.com>
2013-07-19 14:51:27 +0100

    Structures now contain all information needed for email processing.

--- mailer.rb
+++ mailer.rb
@@ -1,15 +1,62 @@
-require_relative 'src/model'
+require_relative 'server'
 
 class Mailer
+  include AutomationStackHelpers
 
   def process
-    jobs = AutomationStack::Infrastructure::Job.all
-    puts "job.id, job.name, job.email_results"
-    
-    jobs.each do |job|
-      puts "#{job.id}, #{job.name}, #{job.email_results}"
+    puts "[#{Time.new}] Processing results for emailing..."
+
+    projects = {}
+    AutomationStack::Infrastructure::Project.all.each { |p| projects[p.name] = {:email => p.email} }
+
+    jobs_awaiting_email = AutomationStack::Infrastructure::Job.where(:email_results => true)
+    queued_or_in_progress = AutomationStack::Infrastructure::Job.where(:status.like('%QUEUED%') | :status.like('%IN PROGRESS%'))
+
+    jobs_awaiting_email.each do |job|
+      project_name = project_name_from_job_name(job.name)
+      if projects.keys.include?(project_name)
+        project = projects[project_name]
+        if project[:ready_jobs].nil?
+          project[:ready_jobs] = [job]
+        else
+          project[:ready_jobs] << job
+        end
+      end
     end
-    puts ""
+
+    projects_with_running_jobs = []
+    queued_or_in_progress.each do |job|
+      project_name = project_name_from_job_name(job.name)
+      if not projects_with_running_jobs.include?(project_name)
+        projects_with_running_jobs << project_name
+      end
+    end
+
+    send_emails(projects, projects_with_running_jobs)
+  end
+  
+  private
+
+  def send_emails(projects, projects_with_running_jobs)
+    projects.each do |proj_name, proj_hash|
+      if proj_hash[:ready_jobs].nil?
+        next
+      end
+      
+      if proj_hash[:ready_jobs].nil? or proj_hash[:ready_jobs].empty?
+        next
+      end
+
+      if not projects_with_running_jobs.include?(proj_name)
+        puts "Emailing #{proj_hash[:email]} with:"
+        proj_hash[:ready_jobs].each do |job|
+          puts "\t#{job.name} -> #{job.status}"
+          # update the email flag on the processed job
+          job.email_results = false
+          job.save
+        end
+      end 
+    end
   end
 
 end

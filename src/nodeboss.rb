0c9ac74e609361381fa9d6ff53515f86dc713ad3
AlexsJones <jonesax@hush.com>
2013-07-23 14:03:47 +0100

    added nodeboss satellite interface

---
+++ src/nodeboss.rb
@@ -0,0 +1,21 @@
+require 'socket'
+class NodeBoss
+	attr_accessor :transaction_api
+	def initialize()
+		@transaction_api="[{CMD`%s}{ID`%s}{DATA`%s}{OTHER`%s}{SENDER`%s}{PORT`%s}]"
+	end
+	def format_instruction(cmd,id,data,other,sender,port)
+		new_string = transaction_api % [cmd,id,data,other,sender,port]
+		return new_string
+	end
+	def send_instruction(hostip, hostport,command)
+		puts "Boss: #{hostip} #{hostport} - #{command}"
+		t = TCPSocket.new(hostip,hostport)
+		t.write(command)
+		t.close
+	end	
+	def machine_reboot(hostip,hostport)
+		b.send_instruction(hostip,hostport,b.format_instruction("SYSTEM","","sudo reboot","","",""))
+	end
+end
+

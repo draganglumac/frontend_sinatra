a640fa318e2e01cf5053f2a731cf0ee53e0012c3
AlexsJones <jonesax@hush.com>
2013-08-21 11:13:36 +0100

    added last job run json

--- src/json_api.rb
+++ src/json_api.rb
@@ -1,12 +1,23 @@
-#get information about a particular job
 get '/api/job/:id' do
-	job_id = params[:id]
 	AutomationStack::Infrastructure::Job.find(:id => params[:id]).to_json	
 end
+get '/api/project/:name' do
+	AutomationStack::Infrastructure::Project.find( :name => params[:name]).to_json
+end
+get '/api/project/lastrun/:name' do
+	projects = AutomationStack::Infrastructure::Project.all
+	
+	projects.each do | s | 
+	
+		if s.name == params[:name]
+			puts "Found project id of #{s.id}"
+	
+			project_jobs = 	AutomationStack::Infrastructure::Job.where(:project_id => s.id)
 
-#TODO
-#Start job
-#Stop job
-#Delete job
-
-#get results
+			res = project_jobs.sort { | x,y | (x.TIMESTAMP || nil) <=> (y.TIMESTAMP || nil) }
+			return res.last.to_json
+		end
+	end
+	
+	return "UNKNOWN"
+end	

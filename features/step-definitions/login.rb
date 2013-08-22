9c08924f1550461896af002f6da252dfd994fe51
draganglumac <dragan.glumac@gmail.com>
2013-08-22 12:26:30 +0100

    Reverting back to sleeps.

--- features/step-definitions/login.rb
+++ features/step-definitions/login.rb
@@ -5,27 +5,24 @@ Given(/^I am on the "Home" screen$/) do
 end
 
 When(/^I press login$/)do
-  sleep(1)
-
+  sleep(2)
   login = @driver.find_element(:id, 'login')
   login.click
 
-  sleep(1)
-
+  sleep(2)
   username = @driver.find_element(:name, 'username')
   password = @driver.find_element(:name, 'password')
+  submit = @driver.find_element(:id, 'sign_in_submit')
 
   username.send_keys "alex.jones"
   password.send_keys "sky"
-
-  submit = @driver.find_element(:id, 'sign_in_submit')
   submit.click
 end
 
 Then(/^I am authenticated$/)do
-  sleep(1)
-
-  text = @driver.find_element(:id, 'login').text
+  sleep(2)
+  login = @driver.find_element(:id, 'login')
+  text = login.text
   text.should include('Logout alex.jones')
   end_session
 end

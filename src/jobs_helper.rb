15c12fe8d05f3d59409a3acdc7f1918b5166fc5f
draganglumac <dragan.glumac@gmail.com>
2013-11-28 08:45:41 +0000

    Fixed a small bug in detection on whether node has any phones or pads.

--- src/jobs_helper.rb
+++ src/jobs_helper.rb
@@ -5,15 +5,15 @@ class Jobhelper
 
   def self.replace_symbols(string,target_machine,device_serial='',device_endpoint='')
     phones = Hound.get_device_ip_from_type_and_machine(1,target_machine)
-    phone_endpoint = phones.nil? ? '' : phones.first['ip'] 
+    phone_endpoint = phones.first.nil? ? '' : phones.first['ip']
     pads =  Hound.get_device_ip_from_type_and_machine(2,target_machine)
-    pad_endpoint = pads.nil? ? '' : pads.first['ip']
+    pad_endpoint = pads.first.nil? ? '' : pads.first['ip']
     puts "Phone endpoint for target machine #{target_machine} is #{phone_endpoint} and Pad endpoint is #{pad_endpoint}"
     
     phones = Hound.get_device_serial_from_type_and_machine(1,target_machine)
-    phone_serial = phones.nil? ? '' : phones.first['serial_number']
+    phone_serial = phones.first.nil? ? '' : phones.first['serial_number']
     pads = Hound.get_device_serial_from_type_and_machine(2,target_machine)
-    pad_serial = pads.nil? ? '' : pads.first['serial_number']
+    pad_serial = pads.first.nil? ? '' : pads.first['serial_number']
     puts "Phone serial for target machine #{target_machine} is #{phone_serial} and Pad serial is #{pad_serial}#"
 
     string.gsub!("$PAD_ENDPOINT",pad_endpoint) if string.include?("$PAD_ENDPOINT")
